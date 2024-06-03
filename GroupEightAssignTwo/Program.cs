39a1edeac6541b61436aeaf9260f823f75b64fbd
AshishGyawali <aashishgyawali60@gmail.com>
2024-06-02 21:21:16 -0400

    initial commit

---
+++ GroupEightAssignTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroupEightAssignTwo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // acessing all data from product class.
+        }
+    }
+}
